53d4123b34c42b261ae1a1275427db0e980b299c
Mariah Morales <mjustine622@gmail.com>
2020-11-10 18:42:13 -0500

    commit tree 94fdba3b652cf998dcc1c03505ecd1781e2691a7

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,47 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+
+    doc = Nokogiri::HTML(URI.open(index_url))
+
+    students = doc.css(".student-card")
+    student_infos = students.collect do |student|
+      {:name => student.css(".student-name").text, :location => student.css(".student-location").text, :profile_url => student.css("a").first["href"]}
+    end
+    # binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    doc = Nokogiri::HTML(URI.open(profile_url))
 
-end
+    main = doc.css("div.main-wrapper.profile")
+
+    social_media_links = main.css(".vitals-container").css("a").collect {|link| link["href"]} 
+
+
+    profile_data = {}
+
+    social_media_links.each do |link|
+      if link.include?("twitter")
+        profile_data[:twitter] = link
+
+      elsif link.include?("linkedin")
+        profile_data[:linkedin] = link 
+
+      elsif link.include?("github")
+        profile_data[:github] = link
+
+      else
+        profile_data[:blog] = link
+      end
+    end
+
+    profile_data[:profile_quote] = main.css("div.profile-quote").text
+    profile_data[:bio] = main.css("div.description-holder p").text
+    # binding.pry
+
+
+    profile_data
+  end 
+end 
 
